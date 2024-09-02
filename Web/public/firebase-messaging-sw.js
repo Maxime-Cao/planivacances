e4d9cdceab02f0a8aa839cfd471ba994f886de5f
Maxime-Cao <m.cao@student.helmo.be>
2024-09-02 14:27:49 +0200

    Update firebase-messaging-sw.js

--- Web/public/firebase-messaging-sw.js
+++ Web/public/firebase-messaging-sw.js
@@ -3,13 +3,13 @@ importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js')
 importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDEed69NWze9I39twdSl8a8SvZAOvO51QU",
-    authDomain: "planivacances.firebaseapp.com",
-    databaseURL: "https://planivacances-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "planivacances",
-    storageBucket: "planivacances.appspot.com",
-    messagingSenderId: "389100630019",
-    appId: "1:389100630019:web:11cfe216ccc49552511e36"
+    apiKey: "...",
+    authDomain: "...",
+    databaseURL: "...",
+    projectId: "...",
+    storageBucket: "...",
+    messagingSenderId: "...",
+    appId: "..."
 };
   
 const app = firebase.initializeApp(firebaseConfig);
